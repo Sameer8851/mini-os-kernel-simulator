e343bed07abc82dd601dceaac716da903b18a38e
SameerChaudhary <Sameerchaudhary8851@gmail.com>
2025-08-04 22:29:40 +0530

    Add interactive CLI for memory manager with fragmentation display

--- src/memory/memory_cli.cpp
+++ src/memory/memory_cli.cpp
@@ -8,63 +8,57 @@ using namespace std;
 void runMemoryCLI() {
     vector<MemoryBlock> memory;
     int totalInternalFragmentation = 0;
+    int n;
 
-    // Initial memory setup (can be customized)
-    memory.push_back(MemoryBlock(-1, 300, false));
+    cout << "\n--- Memory Manager CLI ---\n";
+    cout << "Enter number of memory blocks: " << flush;
+    cin >> n;
 
+    for (int i = 0; i < n; ++i) {
+        int size;
+        cout << "Enter size (in KB) for Block " << i + 1 << ": ";
+        cin >> size;
+        memory.push_back(MemoryBlock(-1, size, false));
+    }
+
+    int choice;
     while (true) {
-        cout << "\n====== Memory Manager CLI ======\n";
-        cout << "1. Allocate Memory\n";
-        cout << "2. Free Memory\n";
-        cout << "3. Print Memory State\n";
-        cout << "4. Exit to Main Menu\n";
-        cout << "Choose an option: ";
-
-        int choice;
+        cout << "\nMemory Operations:\n";
+        cout << " 1. Allocate Memory\n";
+        cout << " 2. Free Memory\n";
+        cout << " 3. Show Memory State\n";
+        cout << " 4. Show Fragmentation\n";
+        cout << " 5. Exit\n";
+        cout << "Enter choice: ";
         cin >> choice;
-
-        if (cin.fail()) {
-            cin.clear(); // Clear input error
-            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Discard invalid input
-            cout << " Invalid input. Please enter a number.\n";
-            continue;
-        }
 
         if (choice == 1) {
             int id, size;
             cout << "Enter Process ID: ";
             cin >> id;
-            cout << "Enter Memory Size to Allocate (KB): ";
+            cout << "Enter Size to Allocate (KB): ";
             cin >> size;
-
-            bool success = allocateMemory(memory, id, size, totalInternalFragmentation);
-            if (!success) {
-                int external = calculateExternalFragmentation(memory, size);
-                if (external > 0) {
-                    cout << " External Fragmentation Detected: " << external << "KB\n";
-                }
-            }
-        }
-
-        else if (choice == 2) {
+            allocateMemory(memory, id, size, totalInternalFragmentation);
+        } else if (choice == 2) {
             int id;
             cout << "Enter Process ID to Free: ";
             cin >> id;
             freeMemory(memory, id);
-        }
-
-        else if (choice == 3) {
+        } else if (choice == 3) {
             printMemoryState(memory);
-            cout << "Total Internal Fragmentation: " << totalInternalFragmentation << "KB\n";
-        }
-
-        else if (choice == 4) {
-            cout << " Returning to Main Menu...\n";
+        } else if (choice == 4) {
+            int reqSize;
+            cout << "Enter size to check fragmentation (KB): ";
+            cin >> reqSize;
+            int external = calculateExternalFragmentation(memory, reqSize);
+            cout << "External Fragmentation: " << external << "KB\n";
+            cout << "Internal Fragmentation: " << totalInternalFragmentation << "KB\n";
+        } else if (choice == 5) {
+            cout << "Exiting Memory Manager...\n";
             break;
-        }
-
-        else {
-            cout << " Invalid option. Please select from 1 to 4.\n";
+        } else {
+            cout << "Invalid choice. Try again.\n";
         }
     }
 }
+
